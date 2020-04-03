1c8cc70f732d52af7a6be6add851dc4da29574f3
sun-23 <twinsamson@gmail.com>
2020-04-03 14:51:26 +0700

    add card to anchor entity scene

--- PairCard-RealityKit/ViewController.swift
+++ PairCard-RealityKit/ViewController.swift
@@ -16,10 +16,41 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
+        // Create an anchor for a horizontal plane with a minimum area of 20 cm^2
+        let anchor = AnchorEntity(plane: .horizontal, minimumBounds: [0.2 ,0.2])
+        arView.scene.addAnchor(anchor)
         
-        // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
+        // Create Cards
+        var allCards: [Entity] = []
+        var cardTemplates: [Entity] = []
+        
+        // Load the model asset for each card
+        for index in 1...8 {
+            let assetName = "modelusdzfile/0\(index)"
+            let cardTemplate = try! Entity.loadModel(named: assetName)
+            cardTemplates.append(cardTemplate)
+        }
+        
+        for cardTemplate in cardTemplates {
+            // Clone each card template twice
+            for _ in 1...2 {
+                allCards.append(cardTemplate.clone(recursive: true))
+            }
+        }
+        
+        // Shuffle the cards so they are randomly ordered
+        allCards.shuffle()
+        
+        // Position the shuffled cards in a 4-by-4 grid
+        for (index, card) in allCards.enumerated() {
+            let x = Float(index % 4) - 1.5
+            let z = Float(index / 4) - 1.5
+            
+            // Set the position of the card
+            card.position = [x * 0.1, 0, z * 0.1]
+            
+            // Add the card to the anchor
+            anchor.addChild(card)
+        }
     }
 }
