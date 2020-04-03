724b75a6bebcefa71f0efe4bb7230e43912edb1e
sun-23 <twinsamson@gmail.com>
2020-04-03 14:17:42 +0700

    Initial Commit

---
+++ PairCard-RealityKit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PairCard-RealityKit
+//
+//  Created by sun on 3/4/2563 BE.
+//  Copyright © 2563 sun. All rights reserved.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
